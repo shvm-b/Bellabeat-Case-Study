7bd476aca31a048980df89beb61342d576c71e8e
shvm-b <59857921+shvm-b@users.noreply.github.com>
2025-12-17 00:58:28 +0530

    Moving "Cleaning - 2 - 2 - Deleting Duplicates - Daily Activity.sql"

--- DailyActivity_Data_Cleaning/Cleaning - 2 - 2 - Deleting Duplicates - Daily Activity.sql
+++ DailyActivity_Data_Cleaning/Cleaning - 2 - 2 - Deleting Duplicates - Daily Activity.sql
@@ -1,11 +1,7 @@
 DELETE FROM `daring-pier-473005-n6.fitbit_fitness_tracker_data.daily_activity` AS main
-
 WHERE EXISTS (
-  
   SELECT 1
-  
   FROM `daring-pier-473005-n6.fitbit_fitness_tracker_data.daily_activity_duplicate_sum` AS temp
-  
   WHERE 
     temp.Id = main.Id
     AND temp.ActivityDate = main.ActivityDate
